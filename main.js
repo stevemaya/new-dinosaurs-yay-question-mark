124ebae102b758abdf5e1be911865098e1161129
Steve Maya <steve.maya@codeimmersives.com>
2020-10-19 21:38:15 -0500

    finished product

--- main.js
+++ main.js
@@ -1,4 +1,32 @@
 // Your code below.
+const makeDino = function (newSpecies, newPeriod, newCarnivore, newExtinct = false) {
+  return {
+      species: newSpecies,
+      period: newPeriod,
+      carnivore: newCarnivore,
+      extinct: newExtinct,
+    };
+};
+
+const makeExtinct = function (dino) {
+  return makeDino(dino.species, dino.period, dino.carnivore, true);
+};
+
+const truncateSpecies = function (dino) {
+  const newDino = makeDino(dino.species, dino.period, dino.carnivore, dino.extinct);
+  if(newDino.species.length >= 10) {
+    newDino.species = newDino.species.slice(0,7) + '...';
+  }
+  return newDino;
+};
+
+const makeSingular = function (dino) {
+  if(dino.species.endsWith('us')) {
+    return makeDino(dino.species.slice(0,-2), dino.period, dino.carnivore, dino.extinct)
+  } else {
+    return dino;
+  }
+};
 
 
 
